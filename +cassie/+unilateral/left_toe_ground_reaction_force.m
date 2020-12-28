1587a14c928009d45805fc420865611830226253
ls <liusong.me@gmai.com>
2020-12-28 18:58:33 +0800

    see #1

--- +cassie/+unilateral/left_toe_ground_reaction_force.m
+++ +cassie/+unilateral/left_toe_ground_reaction_force.m
@@ -1,7 +1,7 @@
 function h_nsf = left_toe_ground_reaction_force(robot)
 % left toe ground reaction force
 GRF = robot.Inputs.ConstraintWrench.fLeftSole;
-h_nsf = UnilateralConstraint(robot, GRF(3), 'leftToeGroundReactionForce', 'fLeftToeBottom');
+h_nsf = UnilateralConstraint(robot, GRF(3), 'leftToeGroundReactionForce', 'fLeftSole');
 
 end
 
